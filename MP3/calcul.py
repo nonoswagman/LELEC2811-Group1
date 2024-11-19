6216061b1266c68dcc4f9f8f153527ed6758dbf8
Loïc <loic.tumelaire@gmail.com>
2024-11-19 17:58:52 +0100

    mp3 finished

--- MP3/calcul.py
+++ MP3/calcul.py
@@ -14,5 +14,15 @@ print("R4 = ", R4)
 ##### NOISE #####
 
 # Filtre, juste du TLV354
+# Puissance du bruit ramené à l'entrée (V**2)/sqrt(Hz) / Gain
+bruitV = np.sqrt((7.5*10**(-9) / 10**(4/20))**2)*39.5
+print("Bruit V = ", bruitV,"nV")
+bruitI = np.sqrt((50*10**(-15) / 10**(4/20))**2)*39.5
+print("Bruit I = ", bruitI,"pI")
 
-# Ampli 1 GIGA formule
+# Ampli 1 GIGA formule (unités pas bonnes ?)
+Aire = 39.5*(280/2)+110*39.5
+print("Aire = ", Aire,"nV*sqrtH(z)")
+KTA = bruitV                                #Pas bon
+Pente = ((1000*10-9)**2 - KTA) *2*np.pi*1   #Pas bon
+print("Pente = ", Pente,"nV**2/Hz")
